fe1584383b6bbf3f5df0af4dd9f534a452495111
cemde <c.emde@me.com>
2023-04-08 19:10:45 +0100

    [skip ci] formatting

--- pcax/nn/link.py
+++ pcax/nn/link.py
@@ -46,9 +46,7 @@ class Link(_Module):
 
 class Linear(Link):
     def __init__(self, in_features: int, out_features: int, bias: bool = True):
-        super().__init__(
-            eqx.nn.Linear, in_features, out_features, bias, key=DEFAULT_GENERATOR()
-        )
+        super().__init__(eqx.nn.Linear, in_features, out_features, bias, key=DEFAULT_GENERATOR())
 
 
 class LayerNorm(Link):
@@ -60,15 +58,57 @@ class LayerNorm(Link):
     ):
         super().__init__(eqx.nn.LayerNorm, shape, eps, elementwise_affine)
 
+
 class Conv(Link):
-    def __init__(self, num_spatial_dims: int, in_channels: int, out_channels: int, kernel_size: Union[int, Sequence[int]], stride: Union[int, Sequence[int]] = 1, padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0, dilation: Union[int, Sequence[int]] = 1, groups: int = 1, use_bias: bool = True):
-        super().__init__(eqx.nn.Conv, num_spatial_dims, in_channels, out_channels, kernel_size, stride, padding, dilation, groups, use_bias, key=DEFAULT_GENERATOR())
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0,
+        dilation: Union[int, Sequence[int]] = 1,
+        groups: int = 1,
+        use_bias: bool = True,
+    ):
+        super().__init__(
+            eqx.nn.Conv,
+            num_spatial_dims,
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+            groups,
+            use_bias,
+            key=DEFAULT_GENERATOR(),
+        )
+
 
 class Conv2d(Conv):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: Union[int, Sequence[int]], stride: Union[int, Sequence[int]] = 1, padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0, dilation: Union[int, Sequence[int]] = 1, groups: int = 1, use_bias: bool = True):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0,
+        dilation: Union[int, Sequence[int]] = 1,
+        groups: int = 1,
+        use_bias: bool = True,
+    ):
         super().__init__(2, in_channels, out_channels, kernel_size, stride, padding, dilation, groups, use_bias)
 
-class MaxPool2d(Link):
-    def __init__(self, kernel_size: Union[int, Sequence[int]], stride: Union[int, Sequence[int]] = 1, padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0, use_ceil: bool = False, **kwargs):
-        super().__init__(eqx.nn.MaxPool2d, kernel_size, stride, padding, use_ceil, **kwargs)
 
+class MaxPool2d(Link):
+    def __init__(
+        self,
+        kernel_size: Union[int, Sequence[int]],
+        stride: Union[int, Sequence[int]] = 1,
+        padding: Union[int, Sequence[int], Sequence[Tuple[int, int]]] = 0,
+        use_ceil: bool = False,
+        **kwargs,
+    ):
+        super().__init__(eqx.nn.MaxPool2d, kernel_size, stride, padding, use_ceil, **kwargs)
