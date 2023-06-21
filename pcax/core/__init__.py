ceb928bd31f42f92c05ad7006e98aa8419d344d1
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2023-06-21 17:53:14 +0000

    Add explicit imports

--- pcax/core/__init__.py
+++ pcax/core/__init__.py
@@ -1,5 +1,37 @@
-from .modules import *
-from .parameters import *
-from .transform import *
-from .filter import *
-from .random import *
+__all__ = [
+    "Module",
+    "Function",
+    "Parameter",
+    "ParameterRef",
+    "ParameterCache",
+    "ParamsDict",
+    "Jit",
+    "Vectorize",
+    "GradValues",
+    "_",
+    "RandomKeyGenerator",
+    "RKG"
+]
+
+
+from .modules import (
+    Module, Function
+)
+from .parameters import (
+    Parameter,
+    ParameterRef,
+    ParameterCache,
+    ParamsDict
+)
+from .transform import (
+    Jit,
+    Vectorize,
+    GradValues
+)
+from .filter import (
+    _
+)
+from .random import (
+    RandomKeyGenerator,
+    RKG
+)
