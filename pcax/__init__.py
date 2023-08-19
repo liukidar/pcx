f1a729d8bde4b80bd734c731a157c64b5f1612e9
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2023-08-19 13:04:11 +0000

    Add default cmd line args

--- pcax/__init__.py
+++ pcax/__init__.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+# Set environment variables
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+for arg in sys.argv:
+    if arg.startswith('--cuda='):
+        os.environ['CUDA_VISIBLE_DEVICES'] = arg.split('=')[1]
+    elif arg == '--xla_preallocate':
+        os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "true"
+
 from .pc import *
 from .core.filter import (
     f
