099cd7b9594e00a8378aa307c4345c8d82462a8b
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2023-08-19 13:01:26 +0000

    Add more layers for equinox

--- pcax/nn/__init__.py
+++ pcax/nn/__init__.py
@@ -1,9 +1,15 @@
 __all__ = [
     "Linear",
     "LayerNorm",
+    "Conv2d",
+    "MaxPool2d",
+    "AvgPool2d"
 ]
 
 from .layer import (
     Linear,
     LayerNorm,
+    Conv2d,
+    MaxPool2d,
+    AvgPool2d
 )
