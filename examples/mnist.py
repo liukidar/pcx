b2fbccb3337b4b525dafdc137e92c4d2cf1d9498
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2023-09-22 10:37:37 +0000

    Update to v0.5.2

--- examples/mnist.py
+++ examples/mnist.py
@@ -6,15 +6,13 @@ import optax
 import pcax as px  # same as import pcax.pc as px
 import pcax.nn as nn
 import pcax.utils as pxu
-from pcax.core import f  # _ is the filter object, more about it later!
+import pcax.core as pxc
+from pcax.core import f
 from pcax.utils.data import TorchDataloader
 
 import numpy as np
 from torchvision.datasets import MNIST
 import timeit
-import os
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 class Model(px.EnergyModule):
@@ -28,10 +26,10 @@ class Model(px.EnergyModule):
         """
         input_layer = nn.Linear(input_dim, hidden_dim)
         self.fc_layers = [input_layer] \
-            + [nn.Linear(hidden_dim, hidden_dim) for _ in range(nm_layers)] \
+            + [nn.Linear(hidden_dim, hidden_dim) for _ in range(nm_layers - 2)] \
             + [nn.Linear(hidden_dim, output_dim)]
 
-        self.pc_nodes = [px.Node() for _ in range(nm_layers + 2)]
+        self.pc_nodes = [px.Node() for _ in range(nm_layers)]
 
         """
         We normally use the x of the last layer as the target, therefore we don't want to update it.
@@ -42,6 +40,7 @@ class Model(px.EnergyModule):
     Here things are a bit different. __call__ accepts an optional target t (used during training),
     which is used to set the x of the last layer.
     """
+    @pxc.pure_fn
     def __call__(self, x, t=None):
         """
         !!! IMPORTANT !!!
@@ -90,6 +89,7 @@ test_dataset = MNIST(
 @pxu.vectorize(filter=f(px.NodeParam, with_cache=True), in_axis=(0,), out_axis=("sum",))
 def train_x(x, *, model):
     model(x)
+
     return model.energy()
 
 
@@ -103,19 +103,24 @@ def train_w(x, *, model):
 
 @pxu.jit()
 def train_on_batch(x, y, *, model, optim_w, optim_x):
+    def x_step(x, *, model, optim_x):
+        with pxu.step(model):
+            g, (v,) = train_x(x, model=model)
+            optim_x(g)
+
     # We are working on the input x, so we initialise the internal nodes with it (this also initialises the cache).
     with pxu.train(model, x, y) as (y_hat,):
-        # model.clear_cache()
-        # flow.scan(x_step)(x, model=model, optim_x=optim_x, length=params["T"])
+        model.clear_cache()
+        pxu.flow.scan(x_step, length=params["T"])(x, model=model, optim_x=optim_x)
 
-        for i in range(params["T"]):
-            # Each forward pass caches the intermediate values (such as activations and energies), so we can use them
-            # to compute the gradients.
-            # px.init_cache takes care of managing the cache.
-            # !!! IMPORTANT: px.init_cache must always be used inside a px.init_nodes context !!!
-            with pxu.step(model):
-                g, (v,) = train_x(x, model=model)
-                optim_x(g)
+        # for i in range(params["T"]):
+        #     # Each forward pass caches the intermediate values (such as activations and energies), so we can use them
+        #     # to compute the gradients.
+        #     # px.init_cache takes care of managing the cache.
+        #     # !!! IMPORTANT: px.init_cache must always be used inside a px.init_nodes context !!!
+        #     with pxu.step(model):
+        #         g, (v,) = train_x(x, model=model)
+        #         optim_x(g)
 
         # !!! IMPORTANT: px.init_cache must always be used inside a px.init_nodes context !!!
         with pxu.step(model):
@@ -131,11 +136,18 @@ def evaluate(x, y, *, model):
         return (y_hat.argmax(-1) == y.argmax(-1)).mean()
 
 
-def epoch(train_fn, dl):
-    for batch in dl:
-        x, y = batch
-        y = one_hot(y, 10)
-        train_fn(x, y)
+def epoch(train_fn, dl=None, train_data=None):
+    if dl is not None:
+        for batch in dl:
+            x, y = batch
+            y = one_hot(y, 10)
+
+            train_fn(x, y)
+    elif train_data is not None:
+        x, y, i = train_data
+
+        for i in range(i):
+            train_fn(x, y)
 
     return 0
 
@@ -153,12 +165,12 @@ def test(evaluate_fn, dl):
 
 if __name__ == "__main__":
     params = {
-        "batch_size": 256,
-        "x_learning_rate": 0.01,
-        "w_learning_rate": 1e-3,
-        "num_epochs": 4,
+        "batch_size": 128,
+        "x_learning_rate": 0.05,
+        "w_learning_rate": 5e-4,
+        "num_epochs": 8,
         "num_layers": 2,
-        "hidden_dim": 128,
+        "hidden_dim": 256,
         "input_dim": 28 * 28,
         "output_dim": 10,
         "T": 4,
@@ -167,7 +179,7 @@ if __name__ == "__main__":
         train_dataset,
         batch_size=params["batch_size"],
         num_workers=8,
-        shuffle=False,
+        shuffle=True,
         persistent_workers=True,
         pin_memory=True,
     )
@@ -193,14 +205,23 @@ if __name__ == "__main__":
             model.parameters().filter(f(px.LayerParam)),
         )
 
+    x, y = next(iter(train_dataloader))
+    y = one_hot(y, 10)
+
     train_fn = train_on_batch.snapshot(model=model, optim_w=optim_w, optim_x=optim_x)
     test_fn = evaluate.snapshot(model=model)
 
-    t = timeit.timeit(lambda: epoch(train_fn, train_dataloader), number=1)
+    t = timeit.timeit(lambda: epoch(train_fn, dl=train_dataloader), number=1)
     print("Compiling + Epoch 1 took", t, "seconds")
 
-    # Time of an epoch (without jitting)
-    t = timeit.timeit(lambda: epoch(train_fn, train_dataloader), number=params["num_epochs"]) / params["num_epochs"]
+    # warmup
+    epoch(train_fn, dl=train_dataloader)
+
+    # Time of an epoch (without having to jit)
+    t = timeit.timeit(
+        lambda: epoch(train_fn, dl=train_dataloader),
+        number=params["num_epochs"]
+    ) / params["num_epochs"]
     print("An Epoch takes on average", t, "seconds")
 
     print("Final Accuracy:", test(test_fn, test_dataloader))
