e63cd052daa0b42b2b69f65a6ee0a1ea6164897c
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-03-21 15:31:10 +0100

    Add serialisation utilities.

--- pcax/utils/__init__.py
+++ pcax/utils/__init__.py
@@ -1,7 +1,16 @@
 __all__ = [
     "step",
     "Optim"
+    
+    "save_params",
+    "load_params"
 ]
 
 from .context import (step)
 from .optim import (Optim)
+
+
+from ._serialisation import (
+    save_params,
+    load_params
+)
