0f2aacd949390e0504b5cb77d2479780d470c4c7
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-04-14 14:24:03 +0000

    Add support for shared gradient between multiple optimizers

--- pcax/utils/optim.py
+++ pcax/utils/optim.py
@@ -6,7 +6,7 @@ import jax.tree_util as jtu
 import equinox as eqx
 
 from ..core._module import BaseModule
-from ..core._parameter import Param, BaseParam, Param
+from ..core._parameter import Param, BaseParam, set, get
 from ..core._static import static
 
 
@@ -17,26 +17,36 @@ class Optim(BaseModule):
         parameters: PyTree | None = None
     ):
         self.optax_opt = static(optax_opt)
-        
+
         if parameters is not None:
             self.init(parameters)
 
     def step(self, module: PyTree, grads: PyTree) -> None:
+        module = eqx.filter(module, self.filter.get(), is_leaf=lambda x: isinstance(x, BaseParam))
+        grads = eqx.filter(grads, self.filter.get(), is_leaf=lambda x: isinstance(x, BaseParam))
+
         updates, state = self.optax_opt.update(
             grads,
             self.state.get(),
-            module
+            module,
         )
         self.state.set(state)
-        
+
         jtu.tree_map(
-            lambda u, p: p.set(eqx.apply_updates(p.get(), u.get())),
+            lambda u, p: set(p, eqx.apply_updates(get(p), get(u))),
             updates,
             module,
             is_leaf=lambda x: isinstance(x, BaseParam)
         )
 
     def init(self, parameters: PyTree) -> None:
+        self.filter = static(jtu.tree_map(
+            lambda x: get(x) is not None,
+            parameters,
+            is_leaf=lambda x: isinstance(x, BaseParam)
+        ))
+        parameters = eqx.filter(parameters, self.filter.get(), is_leaf=lambda x: isinstance(x, BaseParam))
+
         self.state = Param(
             self.optax_opt.init(parameters)
         )
