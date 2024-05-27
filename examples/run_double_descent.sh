63512aef36be2e493773c2903b62ad9cb9f9f973
mcharrak <amine.mcharrak@gmail.com>
2024-05-27 14:08:10 +0000

    new shell script to be able to run any model (pc/bp) with any noise_level of choise as input args to the shell run script

---
+++ examples/run_double_descent.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# usage:
+# ./run_experiments.sh -n 0.2 -s train_double_descent_pc.py
+
+# Default values for parameters
+epochs_list=(100 200 400)
+batch_sizes=(128 256 512)
+learning_rates=(0.01 0.02 0.05)
+noise_level=0.0
+script_name="train_double_descent_bp.py"
+
+# Parse command-line arguments
+while getopts ":n:s:" opt; do
+  case $opt in
+    n) noise_level="$OPTARG"
+    ;;
+    s) script_name="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+        exit 1
+    ;;
+  esac
+done
+
+# Loop over each combination of parameters
+for epochs in "${epochs_list[@]}"; do
+  for learning_rate in "${learning_rates[@]}"; do
+    for batch_size in "${batch_sizes[@]}"; do
+      # Run the Python script with the current set of parameters
+      python $script_name --config ./config.json --noise_level $noise_level --batch_size $batch_size --learning_rate $learning_rate --epochs $epochs
+    done
+  done
+done
