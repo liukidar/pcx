5693ef13bd32097f4c19dc460af11451260d79a5
mcharrak <amine.mcharrak@gmail.com>
2024-06-03 16:24:45 +0000

    temp change to ranges for the epoch values, keep only 1000 epochs

--- examples/run_double_descent.sh
+++ examples/run_double_descent.sh
@@ -4,7 +4,8 @@
 # ./run_double_descent.sh -n 0.2 -s train_double_descent_pc.py
 
 # Default values for parameters
-epochs_list=(500 1000)
+#epochs_list=(500 1000)
+epochs_list=(1000) # temp change
 batch_sizes=(128)
 learning_rates=(0.01 0.02 0.05)
 noise_level=0.0
