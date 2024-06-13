ea8481fdea9af5f534ae1d7738ce652f5a923182
Oleh Lokshyn <olokshyn@gmail.com>
2024-06-13 11:36:04 +0200

    Refactored Autoencoder examples
    1. The Autoencoder examples moved in an appropriate directory structure:
       examples/autoencoders/deconv and examples/autoencoders/linear.
    2. Added examples/README.md that explains the common setup and how to run Autoencoder examples.

--- examples/run_redis.sh
+++ examples/run_redis.sh
@@ -1,1 +1,3 @@
-podman run --rm -p 6379:6379 -v redis-data:/data  docker.io/redis:latest
+container=$(which podman 2>/dev/null || which docker)
+
+$container run --rm -p 6379:6379 -v redis-data:/data  docker.io/redis:latest
