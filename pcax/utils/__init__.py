6f7fc9a9cc4e5f9a825759d99c00b7895e45658f
Luca Pinchetti <luca.pinchetti@cs.ox.ac.uk>
2024-07-05 15:46:02 +0200

    Refactor Mask syntax

--- pcax/utils/__init__.py
+++ pcax/utils/__init__.py
@@ -1,13 +1,15 @@
 __all__ = [
-    "Mask",
-    "m",
+    "M",
+    "M_is",
+    "M_has",
+    "M_hasnot",
     "step",
     "Optim",
     "save_params",
     "load_params",
 ]
 
-from ._mask import Mask, m
+from ._mask import M, M_is, M_has, M_hasnot
 from ._misc import step
 from ._optim import Optim
 
--- pcax/utils/_mask.py
+++ pcax/utils/_mask.py
@@ -1,4 +1,4 @@
-__all__ = ["Mask", "m"]
+__all__ = ["M", "M_is", "M_has", "M_hasnot"]
 
 from typing import Any, Tuple, Dict, Type, Callable
 import types
@@ -9,7 +9,7 @@ from ..core._parameter import Param, BaseParam
 from ..core._tree import tree_ref
 
 
-########################################################################################################################
+#######################################################################################################################
 #
 # MASK
 #
@@ -19,33 +19,51 @@ from ..core._tree import tree_ref
 # but it provides shortcuts to filter based on the type of the parameter or its attributes. Further classes can be
 # defined to provide more complex filtering operations.
 #
-########################################################################################################################
+#######################################################################################################################
 
 
-class Mask:
-    """Mask can either be used to remove unwanted tensors (by setting them to None, similarly to `equinox.filter`) or
-    to map them to a different value based on the whether they pass the filter or not (by using `map_to`). It is
-    important to note that, in any case, the filter acts on the Params of a pytree, seeing them as leaves, while the
-    modified value is the Param's value (and thus it is impossible to differentiate between different children of the
-    same Param, in the case its value is a pytree itself).
+class M:
+    """M applies a mask can either be used to remove unwanted tensors (by setting them to None, similarly to
+    `equinox.filter`) or to map them to a different value based on the whether they pass the filter or not (by setting
+    `map_to` via the `.to` function). It is important to note that, in any case, the filter acts on the Params of a
+    pytree, seeing them as leaves, while the modified value is the Param's value (and thus it is impossible to
+    differentiate between multiple children of the same Param, in the case its value is a pytree itself).
+
+    M that can be used to combine different filters using logical operators (and more).
+    It is necessary as python supports only the `|` operator for types.
+    Available operators are:
+    - `|` for logical or (note that we can also use the `|` operator directly on the type themselves):
+        `float | int` is equivalent to `M(int) | M(float)`. In this case, the parameter will be masked
+        if it is of either type. The other operators behave similarly.
+    - `&` for logical and
+    - `~` for logical not
+    - `has` to filter based on the presence of an attribute with a specific value
+    - `hasnot` to filter based on the absence of an attribute with a specific value
+
+    For example:
+
+    ```python
+    (M(A | B)) & ~M_has(C, attr1=1)
+    ```
+
+    selects paraameters of class A or B that are not of class C and have an attribute `attr1` equal to 1.
     """
 
-    def __init__(self, x: Type | "m" | Callable, map_to: Tuple[Any, Any] | None = None):
+    def __init__(self, mask: Type | types.UnionType | "M" | Callable):
         """Mask constructor.
 
         Args:
-            x (Type | m | Callable): The object on which recursively apply the mask. If it is a type, it will
-                be used to filter the parameters by type. If it is a callable, it must return a boolean and it will be
-                used to filter the parameters by the result of the call. If it is a mask (i.e., `m`), it will be
-                recursively applied to the parameters.
-            map_to (Tuple[Any, Any] | None, optional): if not None, it will be used to set the parameters to either
-                value based on the mask boolean result. If None, the parameters will be set to None if mask is False,
-                and will remain unchanged otherwise. Defaults to None.
+            m (Type | types.UnionType | "mask" | Callable): The object on which recursively apply the mask.
+                If it is a type, it will be used to filter the parameters by type. If it is a callable, it must return
+                a boolean and it will be used to filter the parameters by the result of the call. If it is a mask,
+                it will be recursively applied to the parameters.
         """
-        self.x = x
-        self.map_to = map_to
+        self.mask = mask
+        self.map_to = None
 
-    def __call__(self, pydag: Any, is_pytree: bool = False) -> Any:
+    def __call__(
+        self, pydag: Any, is_pytree: bool = False
+    ) -> Any:
         """Applies the mask to the given pydag.
 
         Args:
@@ -59,13 +77,13 @@ class Mask:
         if self.map_to is None:
 
             def map_fn(param: Any | Param):
-                _mask_value = Mask.apply(self.x, param)
+                _mask_value = self.apply(param)
 
                 return param if _mask_value is True else None
         else:
 
             def map_fn(param: Any | Param):
-                return self.map_to[Mask.apply(self.x, param)]
+                return self.map_to[self.apply(param)]
 
         t = jtu.tree_map(
             map_fn,
@@ -74,9 +92,27 @@ class Mask:
         )
 
         return t
+    
+    def to(self, map_to: Tuple[Any, Any] | None) -> "M":
+        """Sets the `map_to` value, this allows to map the target pytree to the desired pair of masked/unmasked values,
+        instead of relying on the default behaviour of simply filtering out (i.e., substite with `None`) values that
+        do not meet the mask requirements.
+
+        Args:
+            map_to (Tuple[Any, Any] | None): if not None, it will be used to set the parameters to either
+                value based on the mask boolean result. If None, the parameters will be set to None if mask is False,
+                and will remain unchanged otherwise. Defaults to None.
+
+        Returns:
+            M: returns the mask itself to allow for inline epxressions, such as
+                `masked_model = pxu.M(pxnn.LayerParam).to([False, True])(model)`.
+        """
+        self.map_to = map_to
+        
+        return self
 
     @staticmethod
-    def apply(mask, leaf: Any) -> Any:
+    def _resolve(mask: Type | types.UnionType | "M" | Callable, leaf: Any) -> Any:
         """Recursively applies the mask to a leaf.
 
         Returns:
@@ -84,6 +120,8 @@ class Mask:
         """
         if isinstance(mask, type | types.UnionType):
             r = isinstance(leaf, mask)
+        elif isinstance(mask, M):
+            r = mask.apply(leaf)
         elif mask is None:
             r = True
         else:
@@ -91,106 +129,92 @@ class Mask:
 
         return r
 
+    def apply(self, leaf: Any) -> Any:
+        return M._resolve(self.mask, leaf)
 
-class m:
-    """m is a utility mask that can be used to combine different filters using logical operators (and more).
-    It is necessary as python supports only the `|` operator for types.
-    Available operators are:
-    - `|` for logical or (note that we can also use the `|` operator directly on the type themselves):
-        `float | int` is equivalent to `m(int) | m(float)`. In this case, the parameter will be masked
-        if it is of either type. The other operators behave similarly.
-    - `&` for logical and
-    - `~` for logical not
-    - `has` to filter based on the presence of an attribute with a specific value
-    - `has_not` to filter based on the absence of an attribute with a specific value
+    def __invert__(self) -> "_M_not":
+        return _M_not(self)
 
-    For example:
+    def __or__(self, __other) -> "_M_or":
+        return _M_or(self, __other)
 
-    ```python
-    (m(A | B)) & ~m(C).has(attr1=1)
-    ```
+    def __and__(self, __other) -> "_M_and":
+        return _M_and(self, __other)
 
-    selects paraameters of class A or B that are not of class C and have an attribute `attr1` equal to 1.
-    """
+    def has(self, **kwargs) -> "_M_hasattr":
+        return _M_hasattr(self, **kwargs)
 
-    def __init__(
-        self,
-        x: Type | "m" | Callable | None = None,
-    ):
-        self.x = x
-
-    def __call__(self, leaf: Any) -> Any:
-        return Mask.apply(self.x, leaf)
-
-    def __or__(self, __other):
-        return _m_or(self, __other)
-
-    def __and__(self, __other):
-        return _m_and(self, __other)
-
-    def __invert__(self):
-        return _m_not(self)
-
-    def has(self, **kwargs):
-        return _m_hasattr(self, **kwargs)
-
-    def has_not(self, **kwargs):
-        return _m_hasnotattr(self, **kwargs)
+    def hasnot(self, **kwargs) -> "_M_hasnotattr":
+        return _M_hasnotattr(self, **kwargs)
 
 
-class _m_or(Mask):
-    """Computes the logical or between two masks or types"""
-
-    def __init__(self, *args: Tuple[Any, ...]):
-        super().__init__(args)
-
-    def __call__(self, leaf: Any):
-        return any(Mask.apply(i, leaf) for i in self.x)
-
-
-class _m_and(Mask):
-    """Computes the logical and between two masks or types"""
-
-    def __init__(self, *args: Tuple[Any, ...]):
-        super().__init__(args)
-
-    def __call__(self, leaf: Any):
-        return all(Mask.apply(i, leaf) for i in self.x)
-
-
-class _m_not(Mask):
+class _M_not(M):
     """Computes the logical not of a mask or type"""
 
-    def __init__(self, arg: Any):
-        super().__init__(arg)
-
     def __call__(self, leaf: Any):
-        return not Mask.apply(self.x, leaf)
+        return not M._resolve(self.mask, leaf)
 
 
-class _m_hasattr(m):
+class _M_or(M):
+    """Computes the logical or between two masks or types"""
+
+    def __init__(self, *mask: Tuple[Any, ...]):
+        super().__init__(mask)
+
+    def apply(self, leaf: Any):
+        return any(M._resolve(i, leaf) for i in self.mask)
+
+
+class _M_and(_M_or):
+    """Computes the logical and between two masks or types"""
+
+    def apply(self, leaf: Any):
+        return all(M._resolve(i, leaf) for i in self.mask)
+
+
+class _M_hasattr(M):
     """Filters parameters based on their attributes"""
 
-    def __init__(self, arg: m, **attrs: Dict[str, Any]):
-        super().__init__(arg)
+    def __init__(self, mask: M, **attrs: Dict[str, Any]):
+        super().__init__(mask)
         self.attrs = attrs
 
-    def __call__(self, leaf: Any):
-        return Mask.apply(self.x, leaf) and all(
+    def apply(self, leaf: Any):
+        return M._resolve(self.mask, leaf) and all(
             hasattr(leaf, attr) and getattr(leaf, attr) == value
             for attr, value in self.attrs.items()
         )
 
 
-class _m_hasnotattr(m):
+class _M_hasnotattr(_M_hasattr):
     """Filters parameters based on their attributes"""
 
-    def __init__(self, arg: m, **attrs: Dict[str, Any]):
-        super().__init__(arg)
-        self.attrs = attrs
-
-    def __call__(self, leaf: Any):
-        return Mask.apply(self.x, leaf) and all(
+    def apply(self, leaf: Any):
+        return M._resolve(self.mask, leaf) and all(
             (not hasattr(leaf, attr)) or (getattr(leaf, attr) != value)
             for attr, value in self.attrs.items()
         )
+
+
+# Public methods ######################################################################################################
+
+
+def M_is(*mask: Tuple[Any, ...]) -> _M_and:
+    """Creates a mask that filters parameters if they satisfy all the conditions.
+    Equivalent to `M(mask[0]) & M(mask[1]) & ... & M(mask[n])`."""
+
+    return _M_and(*mask)
+
+
+def M_has(mask: M, **attrs: Dict[str, Any]) -> _M_hasattr:
+    """Creates a mask that filters parameters if they satisfy all the conditions.
+    Equivalent to `M(mask).has(**attrs)`."""
+
+    return _M_hasattr(mask, **attrs)
+
+
+def M_hasnot(mask: M, **attrs: Dict[str, Any]) -> _M_hasnotattr:
+    """Creates a mask that filters parameters if they satisfy all the conditions.
+    Equivalent to `M(mask).hasnot(**attrs)`."""
+
+    return _M_hasnotattr(mask, **attrs)
